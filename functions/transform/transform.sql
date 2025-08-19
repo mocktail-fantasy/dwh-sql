0d57e7cfabf364b43037ccdfdd11e688f9c15eaf
Adam Marcus <adam@mac.myfiosgateway.com>
2025-08-19 19:18:44 -0400

    added weekly and annual stats

--- functions/transform/transform.sql
+++ functions/transform/transform.sql
@@ -1,13 +1,12 @@
 CREATE OR REPLACE PROCEDURE transform_raw_data() AS $$
 BEGIN
     -- Call other stored procedures or functions as needed
-    -- For example:
-    -- CALL some_other_stored_procedure();
-    -- CALL another_function();
     CALL load_from_s3();
     CALL insert_derived_odds();
     CALL insert_odds_by_team();
     CALL refresh_active_rosters();
+    CALL load_weekly_player_stats();
+    CALL load_annual_player_stats();
 
 
 END;
