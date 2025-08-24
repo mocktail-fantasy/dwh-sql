60ce85417b58e67a6a512d23a9b91708abb794b9
Adam Marcus <adam@mac.myfiosgateway.com>
2025-08-23 21:15:06 -0400

    dummy commit

--- procedures/etl/logs.sql
+++ procedures/etl/logs.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS etl.job_logs (
     log_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     procedure_name TEXT NOT NULL,
-    status TEXT NOT NULL, -- e.g., 'STARTED', 'COMPLETED', 'FAILED'
+    status TEXT NOT NULL, -- 'STARTED', 'COMPLETED', 'FAILED'
     message TEXT,
     log_time TIMESTAMPTZ DEFAULT now()
 );
